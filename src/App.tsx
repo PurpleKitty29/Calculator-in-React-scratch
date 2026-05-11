db2f20e7fea1ed1c68fd3e2404570093c8bc4d61
Cr1stal_Off1cial <tanigawagamer@gmail.com>
2026-05-11 12:11:44 +0100

    Add Error 404 customizable screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Calculator from './pages/calculatorCode';
+import Error404 from './pages/Error404';
 
 function App() {
   return (
     <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Calculator />} />
+        <Route path="/*" element={<Error404 />} />
       </Routes>
     </>
   );
