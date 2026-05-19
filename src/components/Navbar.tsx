5cfc6f1a47d24fbb8717973d08eb0d44687078db
Cr1stal_Off1cial <tanigawagamer@gmail.com>
2026-05-19 16:40:07 +0100

    Fixed Navbar URLs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,14 @@ function Navbar() {
   return (
     <nav className="navbar">
       <NavLink
-        to="/"
+        to="/Calculator-in-React-scratch/"
         end
         className={({ isActive }) => (isActive ? 'linkOn' : 'linkOff')}
       >
         Calculator
       </NavLink>
       <NavLink
-        to="/history"
+        to="/Calculator-in-React-scratch/history"
         className={({ isActive }) => (isActive ? 'linkOn' : 'linkOff')}
       >
         History
