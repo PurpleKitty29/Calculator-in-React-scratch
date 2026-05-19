c4cef2914542616d992b5fe55c4a3c3b5a3eff8c
Cr1stal_Off1cial <tanigawagamer@gmail.com>
2026-05-19 15:49:32 +0100

    changed the CV-0.png path

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import '../App.css';
+import dizzyImg from '../images/CV-0 dizzy.png';
 
 function Error404() {
   return (
     <div>
       <h1 className="errorTitle">404</h1>
 
-            <img src="/CV-0 dizzy.png" alt="dizzy" className="dizzyPic" />
+      <img src={dizzyImg} alt="dizzy" className="dizzyPic" />
 
       <p>Oopsie! Looks like you're looking for the void!</p>
       <p>
